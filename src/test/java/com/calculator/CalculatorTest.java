f78d9c7c7f7e749f299ce24c774d6bbdbf76d50d
Ken Klein <tka.klein.public@gmail.com>
2025-07-17 23:22:36 -0400

    Assert true passes

---
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest 
+{
+
+    @Test
+    void failTest() 
+    {
+        assertTrue(true, "This test should always pass");
+    }    
+}
