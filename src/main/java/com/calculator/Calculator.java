52c694d4940ba0b3983ae8384a659096ce4ff37b
Ken Klein <tka.klein.public@gmail.com>
2025-07-18 15:35:47 -0400

    add working add tests

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.calculator;
+
+public class Calculator 
+{
+
+    public int add(int operand1, int operand2) 
+    {
+        return operand1 + operand2;
+    }
+    
+}
--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -4,12 +4,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
 public class CalculatorTest 
 {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() 
+    {
+        calculator = new Calculator();
+    }
 
     @Test
-    void failTest() 
+    void GivenTwoPosInts_WhenAdd_ThenProvideSum() 
     {
-        assertTrue(true, "This test should always pass");
-    }    
+        assertThat(this.calculator.add(2,3), is(5));
+    }
+
+    @Test
+    void GivenTwoNegativeInts_WhenAdd_ThenProvideSum() 
+    {
+        assertThat(this.calculator.add(-2,-3), is(-5));
+    }
+
 }
